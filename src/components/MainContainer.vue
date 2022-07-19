96a0f7de5733bbded21c7c446e7d471bc8164c55
Esma Engin <esmaengin91@gmail.com>
2022-07-19 14:52:45 +0300

    first commit

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <p>{{ text }}</p>
+        <p>Lorem ipsum dolor sit amet</p>
+    </div>
+</template>
+
+<script>
+export default{
+  name:"MainContainer",
+  data() {
+    return{
+      text: "Lorem ipsum dolor sit amet"
+    }
+  },
+  methods : {},
+  computed: {},
+  watch: {}
+}
+</script>
+<style>
+
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="product">
+        <strong>{{ product.title }}</strong>
+        <p>{{ product.description }}</p>
+        <strong>Fiyat : {{ product.price }} | Adet : {{ product.count }}</strong>
+        <br><br>
+        <button @click="addToCart(product)">Sepete Ekle</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "productItem",
+    props : ["product"],
+    methods: {
+        addToCart(){
+            this.$emit("addToCart", this.product)
+        }
+    }
+}
+</script>
+
+<style>
+.product{
+    border: 2px dashed #666;
+    padding: 5px 10px;
+    width: 300px;
+    margin-bottom: 5px;  
+}
+
+.product:hover{
+    border: 2px solid #666;
+    transition: all 1s;
+}
+</style>
